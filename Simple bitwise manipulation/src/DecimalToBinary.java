cf2b50d3fefb4e4e7ed6319f57618c688943dd9f
itsMO253 <mwarsame34@outlook.com>
2021-02-06 10:37:26 -0800

    extra practice learned how to covert Decimal Number to Binary

---
+++ Simple bitwise manipulation/src/DecimalToBinary.java
@@ -0,0 +1,18 @@
+// convert int to String --> String.valueOf(insert a int)
+// convert String to int --> Integer.parseInt(insert a string)
+public class DecimalToBinary {
+	
+	public static void main(String[] args) {
+		int num = 5; 
+		String result = "";
+		
+		// you want it to stop at ZERO
+		while(num != 0) {
+			result = result + String.valueOf(num % 2); // concacenting the Binary number to string 
+			num = num /2; // moving to the next value on the left
+		}
+		int finalResult = Integer.parseInt(result);
+		System.out.println(finalResult);
+	}
+
+}
