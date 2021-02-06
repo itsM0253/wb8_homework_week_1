eec576b5399755b9f794f79b8129aa4f0efc6e6f
itsMO253 <mwarsame34@outlook.com>
2021-02-06 10:38:39 -0800

    Completed reverseString()

---
+++ Simple String Manipulation/src/ReverseString.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class ReverseString {
+	
+	public static void main(String[] args) {
+		char[] s = {'h', 'e', 'l', 'l', 'o'};
+		reverseString(s);
+		
+		System.out.println(Arrays.toString(s));
+		
+	}
+	
+	public static void reverseString(char[] s) {
+		int j = s.length - 1;
+		int i = 0;
+		while(i < j) {
+			char temp = s[i]; // make sure it's a character not int because of ascii value
+			s[i] = s[j];
+			s[j] = temp;
+			j--;
+			i++;
+		}
+		
+	}
+}
