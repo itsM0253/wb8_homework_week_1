3e00243b6c4e237d0d1831605314650db78329cb
itsMO253 <mwarsame34@outlook.com>
2021-02-07 13:16:54 -0800

    learned 2D arrays work in java

---
+++ Arrays/Arrray2DPractice.java
@@ -0,0 +1,17 @@
+
+public class Arrray2DPractice {
+
+	public static void main(String[] args) {
+		int[][]  arr = { {2,4,-1},
+			    				 {-10,5,4},
+			    				 {18,-7,6}};
+		
+		for(int i = 0; i < arr.length; i++) { // row
+			for(int j = 0; j < arr.length; j++) { // column
+				System.out.println(arr[i][j]);
+			}
+		}
+
+	}
+
+}
