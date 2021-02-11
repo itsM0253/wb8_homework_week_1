f8bda7bbf92ca27f73311eb988a300d75b4184e7
itsMO253 <mwarsame34@outlook.com>
2021-02-10 18:15:21 -0800

    Did a little practice to master indexOf()

---
+++ Simple String Manipulation/IndexOfPractice.java
@@ -0,0 +1,15 @@
+
+public class IndexOfPractice {
+
+	public static void main(String[] args) {
+		String x = "Karl Hadwen";
+		String y = "Karl";
+		String z = "Hadwen";
+		
+		System.out.println("Found Index :" + x.indexOf('a'));
+		System.out.println("Found Index :" + x.indexOf('a',4)); // Find letter a starting from index 4
+		System.out.println("Found Index :" + x.indexOf(y));
+		System.out.println("Found Index :" + x.indexOf(z));
+	}
+
+}
