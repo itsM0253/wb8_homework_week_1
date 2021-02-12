3b3e0310437f3bd05fa19de3afb7edf0b5c1aaf9
itsMO253 <mwarsame34@outlook.com>
2021-02-12 00:47:05 -0800

    Completed the reverse words with spaces in between

---
+++ Simple String Manipulation/ReverseWords576.java
@@ -0,0 +1,25 @@
+
+public class ReverseWords576 {
+
+	public static void main(String[] args) {
+		String word = "Let's take LeetCode contest";
+		String answer = reverseWords(word);
+		System.out.println(answer);
+
+	}
+	
+	public static String reverseWords(String str) {
+		String[] words = str.split(" ");
+		for(int i = 0; i < words.length; i++) {
+			String word = words[i];
+			words[i] =  new StringBuilder(word).reverse().toString() + " "; // reversing word by word inPlace
+		}
+		StringBuilder sb = new StringBuilder();
+		//traverse through the words array
+		for(String s: words) {
+			sb.append(s);
+		}
+		return sb.toString().trim(); // to get rid of the space at the end
+	}
+
+}
