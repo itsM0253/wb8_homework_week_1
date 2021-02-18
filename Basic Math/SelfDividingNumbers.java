0c6e5618bfb8f4476ac2f817b266dc9b3c7b7b15
itsMO253 <mwarsame34@outlook.com>
2021-02-18 01:19:03 -0800

    Completed selfDivide problem

---
+++ Basic Math/SelfDividingNumbers.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+
+public class SelfDividingNumbers {
+
+	public static void main(String[] args) {
+		int left = 1;
+		int right = 22;
+		ArrayList<Integer> nums = selfDivide(left,right);
+		System.out.println(nums);
+
+	}
+	
+	public static ArrayList<Integer> selfDivide(int left, int right) {
+		// create output array list
+        // loop through while left <= right
+        // if helper returns true, add left to list
+        // increment left
+        // return output
+        
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        while(left <= right) {
+            if(helper(left)) { // if it's true
+                list.add(left);
+            }
+            left++;
+        }
+        return list;
+    }
+	
+	 private static boolean helper(int num) {
+	        // create a temporary variable to manipulate
+	        // take last digit of num, if it is 0 or is not divisible by num return false
+	        // pop off last digit off temp
+	        // otherwise if we pass the selfDivide checks, return true
+	        
+	        int temp = num;
+	        while(temp != 0) {
+	            int lastDigit = temp % 10; // popping off last value
+	            
+	            // These are the 2 cases it is not a self dividing number 
+	            if(lastDigit == 0 || num % lastDigit != 0) { // example: 128 % 8 = 0
+	                return false;
+	            }
+	            temp = temp / 10;
+	        }
+	        return true;
+	    }
+	
+	
+	
+
+}
